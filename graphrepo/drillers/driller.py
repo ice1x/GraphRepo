2460665b87b79d8c932fccf3e99d4ffccb5dc1c9
ice x <ice2600x@gmail.com>
2025-07-27 14:46:12 +0300

    Roll back pydriller

--- graphrepo/drillers/driller.py
+++ graphrepo/drillers/driller.py
@@ -19,13 +19,7 @@ from diskcache import Cache
 from datetime import datetime
 from py2neo import Graph
 # PyDriller recently reorganized modules, try all known import locations
-try:
-    from pydriller import RepositoryMining
-except ImportError:
-    try:  # PyDriller 2.x
-        from pydriller.repository_mining import RepositoryMining
-    except ImportError:  # PyDriller 3.x
-        from pydriller.git_mining import RepositoryMining
+from pydriller import RepositoryMining
 
 import graphrepo.utils as utl
 import graphrepo.drillers.batch_utils as b_utl
